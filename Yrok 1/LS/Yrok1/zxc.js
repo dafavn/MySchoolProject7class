0339599c690e3fd2c7427e0b317092353015bdec
dafavn <125496467+dafavn@users.noreply.github.com>
2023-02-22 18:07:14 +0300

    My Projects!

---
+++ Yrok 1/LS/Yrok1/zxc.js
@@ -0,0 +1,36 @@
+
+// let bambambam = 'da ya pozer i chto'
+// alert(bambambam +' vbhg')
+
+//-----------------------------------------
+
+ //let name = prompt('эээээээ брат пррекащая уже ето а!' , 'нет')
+ //let ing = prompt ('hahahah' , 'sam')
+ //let hok = prompt ('hog 2 and 6' , 'AAAAAA YIDI OTDYDA') 
+ //alert('Privet ya dictor is masterskaya nastroenia' + name +  ing +  hok)
+//alert(`gg team rakov, ${name}. ti lox${ing} . ya${hok}.`)
+//-----------------------------------------------
+
+// let s1 = Number(prompt('Vedite skolko vi xotite let sebe' , 0))
+// let s2 = Number(prompt('Vedite skolko vi xotite let svoei sobake' , 0))
+// let s3 = Number(prompt('Vedite skolko vi xotite let koty' , 0))
+// let s4 = Number(prompt('Vedite skolko vi xotite let credity' , 0))
+
+
+// let s1 = +prompt('Vedite skolko vi xotite let sebe' , 0)
+// let s2 = +prompt('Vedite skolko vi xotite let svoei sobake' , 0)
+// let s3 = +prompt('Vedite skolko vi xotite let koty' , 0)
+// let s4 = +prompt('Vedite skolko vi xotite let credity' , 0)
+
+// let result = s1+s2+s3+s4
+// alert(result)
+
+// alert(('b' + 'a' + +'a' + 'a').toLowerCase)
+
+// alert(5 + 2)
+// alert(5 - 2)
+// alert(5 * 2)
+// alert(5 / 2)
+
+// alert(5 ** 2)
+// alert(5%2)
